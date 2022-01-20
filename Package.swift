4c7db9566edd91312ac0d793e6c6399430012735
Pontus Jacobsson <pontus.jacobsson@visibacare.com>
2022-01-20 08:12:09 +0100

    Added SPM Package file

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "SocketRocket",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_10),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "SocketRocket",
+            targets: ["SocketRocket"]),
+        .library(
+            name: "SocketRocket-Static",
+            type: .static,
+            targets: ["SocketRocket"]),
+        .library(
+            name: "SocketRocket-Dynamic",
+            type: .dynamic,
+            targets: ["SocketRocket"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "SocketRocket",
+            dependencies: [],
+            path: "SocketRocket",
+            exclude: ["Resources/Info.plist"],
+            publicHeadersPath: "SocketRocket"
+        )
+    ]
+)
